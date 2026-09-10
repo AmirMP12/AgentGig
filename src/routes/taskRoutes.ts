e4699676701e1c573d0a40d0d93df7f5c3be0328
AmirMP12 <ampanahi17@gmail.com>
2026-09-10 13:49:57 +0330

    update taskRoutes

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -5,12 +5,25 @@ import {
   verifyPaymentHandler,
   listWorkersHandler,
 } from '../controllers/taskController';
+import {
+  listTransactionsHandler,
+  getTaskTransactionsHandler,
+} from '../controllers/transactionController';
+import { taskRepository } from '../database/taskRepository';
 
 const router = Router();
 
+// Task Management
 router.post('/tasks', createTaskHandler);
+router.get('/tasks', (_req, res) => res.json(taskRepository.getAllTasks()));
 router.get('/tasks/:id', getTaskHandler);
 router.post('/tasks/:id/verify-payment', verifyPaymentHandler);
+
+// Worker Registry
 router.get('/workers', listWorkersHandler);
+
+// Transaction Ledger (Milestone 2 Feature)
+router.get('/transactions', listTransactionsHandler);
+router.get('/tasks/:taskId/transactions', getTaskTransactionsHandler);
 
 export default router;
